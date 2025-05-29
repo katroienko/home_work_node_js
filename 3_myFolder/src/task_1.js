b678216bae24b930da7728f67da96d27c7a3b85a
Roienko Kateryna <roienkokateryna@gmail.com>
2025-05-29 12:33:11 +0200

    add nodemon

--- 3_myFolder/src/task_1.js
+++ 3_myFolder/src/task_1.js
@@ -1,2 +1,21 @@
-import fs from 'node:fs/promises';
 
+const fsCallback = require('fs');
+const folderName = 'myFolder';
+
+// Create directory
+fsCallback.mkdir(folderName, (err) => {
+  if (err) {
+    console.error('Error creating folder:', err);
+  } else {
+    console.log(`Folder "${folderName}" created successfully.`);
+
+    // Remove directory (after successful creation)
+    fsCallback.rmdir(folderName, (err) => {
+      if (err) {
+        console.error('Error removing folder:', err);
+      } else {
+        console.log(`Folder "${folderName}" removed successfully.`);
+      }
+    });
+  }
+});
