0ca838b69ff0569ba54f7409347ec56883ed05b2
Roienko Kateryna <roienkokateryna@gmail.com>
2025-07-26 18:25:09 +0200

    add 13 hw

---
+++ 13_hw_conections/src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+
+import notFoundHandler from "./middlewares/notFoundHandler.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
+// import Publisher from "./db/Publisher.js";
+// import Magazine from "./db/Magazine.js";
+// import Tag from "./db/Tag.js";
+// import Article from "./db/Article.js";
+
+
+const startServer = () => {
+    const app = express();
+
+    app.use(cors());
+    app.use(express.json());
+
+
+
+    app.use(notFoundHandler);
+    app.use(errorHandler);
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`Server running on ${port} port`));
+};
+
+export default startServer;
